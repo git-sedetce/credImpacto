0a9a1a56bee54835559dd685674b8a96d182d8fc
Daniel Oliveira Araujo <danieloliveira14@gmail.com>
2026-07-21 16:42:45 -0300

    ajuste de fontes

---
+++ client/src/app/componentes/credImpacto/what-is/what-is.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { WhatIsComponent } from './what-is.component';
+
+describe('WhatIsComponent', () => {
+  let component: WhatIsComponent;
+  let fixture: ComponentFixture<WhatIsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [WhatIsComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(WhatIsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
