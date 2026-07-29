36b8782f215eac304d5f4885b357c8306d5db982
Daniel Oliveira Araujo <danieloliveira14@gmail.com>
2026-07-29 13:50:19 -0300

    criando componente fale conosco, trabalhar com ele, se possível, depois

---
+++ client/src/app/componentes/credImpacto/fale-conosco/fale-conosco.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fale-conosco',
+  standalone: false,
+  templateUrl: './fale-conosco.component.html',
+  styleUrl: './fale-conosco.component.css'
+})
+export class FaleConoscoComponent {
+
+}
