112ea1b0187ba6e6650e1da3878c51c7808767ba
Daniel Oliveira Araujo <danieloliveira14@gmail.com>
2026-08-05 10:21:23 -0300

    correção de validação de telefone

--- client/src/app/componentes/credImpacto/directives/telefone-validacao.directive.ts
+++ client/src/app/componentes/credImpacto/directives/telefone-validacao.directive.ts
@@ -7,7 +7,7 @@ import {
 } from '@angular/forms';
 
 @Directive({
-  selector: '[appTelefoneValidacao][ngModel]',
+  selector: '[appTelefoneValidacao]',
   providers: [
     {
       provide: NG_VALIDATORS,
